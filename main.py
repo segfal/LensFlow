80d0148d2604d7e5955dcf027affeb3998430718
segfal <segfaultboi@gmail.com>
2023-12-27 23:56:21 -0500

    j change

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
-from File_Functions import file_exists,metadata,file_filter,file_mover
+'''from psysort/File_Functions import file_exists,metadata,file_filter,file_mover
+'''
+import psutil
 
-pathOfUSB = ""
-pathOfPics = ""
+PICTURES_LIBRARY_PATH = "F:\Pictures\Raws"
+REMOVABLE_USB_PATH = None  
 
-def get_usb_drives():
+def get_usb_drive():
+    global REMOVABLE_USB_PATH
     drive_list = []
     for partition in psutil.disk_partitions():
         if 'removable' in partition.opts:
             drive_list.append(partition.mountpoint)
-    return drive_list
+    if drive_list:
+        REMOVABLE_USB_PATH = drive_list[0]
+        
+usb_drives = get_usb_drive()
+
+def some_function():
+    # You can use the global variables here
+    print(f"Accessing pictures library at {PICTURES_LIBRARY_PATH}")
+    if REMOVABLE_USB_PATH:
+        print(f"Accessing removable USB at {REMOVABLE_USB_PATH}")
+    else:
+        print("No removable USB drive found.")
+
+print(f"Removable USB Path Set To: {REMOVABLE_USB_PATH}")
 
 # Now let's assume the first removable drive is the USB you want to use
-usb_drives = get_usb_drives()
 if usb_drives:
     REMOVABLE_USB_PATH = usb_drives[0]
 else:
     REMOVABLE_USB_PATH = None  # Handle the case where no USB is found
+
+
